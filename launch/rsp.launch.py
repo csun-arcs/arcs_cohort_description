ba23a9da38126826366120094e8b31a5d13528e2
pikaic <pikaic2010@gmail.com>
2025-02-25 15:18:06 -0800

    added rviz and jsp nodes to launch file

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,15 +12,14 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    
     use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # Process the URDF file
+    
     pkg_path = os.path.join(get_package_share_directory('arcs_cohort_description'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
+    rviz_config_file = os.path.join(pkg_path, 'rviz_config', 'robot_model.rviz')
     
-    # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -29,13 +28,27 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
 
-    # Launch!
+    node_rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        node_joint_state_publisher_gui,
+        node_rviz2
     ])
